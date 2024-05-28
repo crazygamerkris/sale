acdf01dc8041fce845803bd59993b1ccb688de32
crazygamerkris <crazygamerkris9@gmail.com>
2024-05-28 19:20:08 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const paintings = {
-        
-    }
     const gallery = document.getElementById('gallery');
-
-    paintings.forEach(painting => {
-        const paintingElement = document.createElement('a');
-        paintingElement.classList.add('painting');
-        paintingElement.href = painting.detailsPage;
-
-        paintingElement.innerHTML = `
-            <img src="${painting.imageUrl}" alt="${painting.title}">
-            <div class="painting-details">
-                <h2>${painting.title}</h2>
-                <p>Description: ${painting.description}</p>
-                <p>Price: ${painting.price}</p>
-            </div>
-        `;
-
-        gallery.appendChild(paintingElement);
-    });
 });
